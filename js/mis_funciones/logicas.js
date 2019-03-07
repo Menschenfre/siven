260d260ccf04152389644828cad5be658a0be1be
Matias Huilipan Saez <exmhusa@dom_bci_prod.cl>
2019-03-07 10:44:17 -0300

    1 Video agregado

--- js/mis_funciones/logicas.js
+++ js/mis_funciones/logicas.js
@@ -32,7 +32,8 @@ $(document).ready(function(){
     	"https://www.youtube.com/embed/71Gt46aX9Z4",
     	"https://www.youtube.com/embed/UiHmeHZAc0s",
     	"https://www.youtube.com/embed/HzTlB-TjAzM",
-        "https://www.youtube.com/embed/yyDUC1LUXSU"
+        "https://www.youtube.com/embed/yyDUC1LUXSU",
+        "https://www.youtube.com/embed/pBkHHoOIIn8"
     	];
   		var playerDiv = document.getElementById("random_player");
         var player = document.createElement("IFRAME");
