4d28778b7a2502d04e18aca9f3488fe0411b6b03
Matias Huilipan Saez <exmhusa@dom_bci_prod.cl>
2019-03-07 12:50:38 -0300

    fix otro video

--- js/logicas.js
+++ js/logicas.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
     	"https://www.youtube.com/embed/UiHmeHZAc0s",
     	"https://www.youtube.com/embed/HzTlB-TjAzM",
         "https://www.youtube.com/embed/eAVl2cpFKyQ",
-        "https://www.youtube.com/watch?v=H1KBHFXm2Bg"
+        "https://www.youtube.com/embed/H1KBHFXm2Bg"
     	];
   		var playerDiv = document.getElementById("random_player");
         var player = document.createElement("IFRAME");
