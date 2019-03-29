b94648ef1ace398eb317bf426548056582847753
Sivenatico <sivenatico@siven.cl>
2019-03-29 00:59:23 -0400

    primo query funcionando

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -7,7 +7,10 @@ $servidor = "localhost";
 $basededatos = "sivenati_siven";
 
 //Creamos la conexión a la base de datos
-$conexion = mysqli_connect($servidor, $usuario, $pass);
+$conexion = mysqli_connect($servidor, $usuario, $pass) or die ( "Algo ha ido mal con la conexión a la base de datos");
+
+//Seteamos a utf8 para carácteres especiales
+mysqli_set_charset($conexion,"utf8");
 
 //Seleccionamos la bd
 $db = mysqli_select_db($conexion, $basededatos);
@@ -17,4 +20,8 @@ $query = "SELECT * FROM usuarios";
 
 // Ejecutamos la consulta
 $resultado = mysqli_query($conexion, $query) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+// Cerramos la conexión posterior a la consulta
+mysqli_close($conexion);
+
 ?>
