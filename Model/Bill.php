64642f5038a49997c2e682ee90030a0cf6e3353a
Sivenatico <sivenatico@siven.cl>
2019-05-03 00:29:51 -0400

    bill alfa

---
+++ Model/Bill.php
@@ -0,0 +1,10 @@
+<?php  
+require_once ('Crud.php');
+
+class Bill extends Crud{
+	private $id;
+	private $category;
+	private $total;
+}
+
+?>
--- View/Includes/url.php
+++ View/Includes/url.php
@@ -3,7 +3,6 @@
 $Base_url="/home2/sivenati/public_html";
 $Assets_origen= "/Assets";
 $View_origen= "/View";
-$Function_origen= "/Function";
 //Obtiene el nombre del archivo actual.
 //$page= basename( __FILE__ );
 // Includes base URL
@@ -18,8 +17,6 @@ $Materialdesign_url= "/Fonts/Materialdesignicons-3.6.95/css/materialdesignicons.
 $css = "${Includes_url}/css.php";
 // Includes js
 $js = "${Includes_url}/js.php";
-// Includes function
-$function = "${Includes_url}/function.php";
 
 /*----------------------------------------------------------*/
 // Assets css
