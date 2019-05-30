c0fb73e9d69b4d449ee8577e770aa88b607b4e62
Matias Huilipan Saez <exmhusa@dom_bci_prod.cl>
2019-05-30 18:29:37 -0400

    Avance producto

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -10,7 +10,7 @@ class ProductController{
 	 */
 	public function __construct(){
 
-		
+		 
 		 
 	}
 
--- View/Administrative/Bills/add_product.php
+++ View/Administrative/Bills/add_product.php
@@ -1,49 +1,73 @@
 <div class="col-md-7 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">Ingreso de productos</h4>
-                  <p class="card-description">
-                    Acá se registran los gastos por productos
-                  </p>
-                  
-                  <div class="form-group">
-                    <label>Default input</label>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username">
-                  </div>
-                  
-                </div>
-                <div class="card-body">
-                  <h4 class="card-title">Selectize</h4>
-                  <div class="form-group">
-                    <label for="exampleFormControlSelect1">Large select</label>
-                    <select class="form-control form-control-lg" id="exampleFormControlSelect1">
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleFormControlSelect2">Default select</label>
-                    <select class="form-control" id="exampleFormControlSelect2">
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleFormControlSelect3">Small select</label>
-                    <select class="form-control form-control-sm" id="exampleFormControlSelect3">
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">Ingreso de productos</h4>
+      <p class="card-description">
+        Acá se registran los gastos por productos
+      </p>
+      
+      <div class="form-group">
+        <label>Nombre</label>
+        <input type="text" id="prod_name" class="form-control form-control-lg" placeholder="Nombre" aria-label="Nombre">
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlSelect1">Categoría</label>
+        <select class="form-control form-control-lg" id="prod_category">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label>Cantidad</label>
+        <input type="text" id="prod_total" class="form-control form-control-lg" placeholder="Cantidad" aria-label="Cantidad">
+      </div>
+
+      <div class="form-group">
+        <label>Precio</label>
+        <input type="text" id="prod_price" class="form-control " placeholder="Precio" aria-label="Precio">
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+  
+function login(){
+
+  //Capturamos las id de los input
+      var prod_name = $("#prod_name").val();
+      var prod_category = $("#prod_category").val();
+      var prod_total = $("#prod_total").val();
+      var prod_price = $("#prod_price").val();
+
+
+      $.ajax({
+          //datos que se envian a traves de ajax, primer valor nombre de la variable, segundo valor del input declarado previamente
+              data:  {"prod_name":prod_name,"prod_category":prod_category,"prod_total":prod_total,"prod_price":prod_price}, 
+              url:   '/View/Bills/index.php', //archivo que recibe la peticion
+              type:  'post', //método de envio
+              beforeSend: function () {
+                  alert("Logeando...");
+                      //$("#resultado").html("Procesando, espere por favor...");
+              },
+              //response es lo primero que se retorna en el controller
+              success:  function (response) { //una vez que el archivo recibe el request lo procesa y lo devuelve
+
+            //Si el controlador retorna un positivo se devuelve mensaje exitoso 
+                  if(response==1){
+                      alert("Sesión activa");
+                      window.location = "/admin";
+
+                  }else{
+                    alert("No existe usuario, sesión fallida");
+                  }
+                      
+              }
+      });
+}
+</script>
